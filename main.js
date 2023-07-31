bcbec7edf2e8403d134694893673057fcb0ded26
Danilosky <danilohei2002@gmail.com>
2023-07-31 17:06:58 -0500

    Fucion de index8 V3

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
 const manuEmail = document.querySelector(".navbar-email");
 const DesktopMenu = document.querySelector(".desktop-menu");
+
+const menuCarrito = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+
 const IconButton = document.querySelector(".menu");
 const menuMobile = document.querySelector(".mobile-menu");
 
 manuEmail.addEventListener("click", toggleDesktopMenu);
 IconButton.addEventListener("click", toggleMobileMenu);
+menuCarrito.addEventListener("click", toggleCarritoDeCompras)
+
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains("inactive")
+    if(!isAsideClosed){
+        aside.classList.add("inactive")
+    }
     DesktopMenu.classList.toggle("inactive");
 }
 
+
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if(!isAsideClosed){
+        aside.classList.add("inactive")
+    }
     menuMobile.classList.toggle("inactive");
+
+}
+
+
+function toggleCarritoDeCompras (){
+    //aside.classList.toggle("inactive");
+    const isMovileMenuClosed = menuMobile.classList.contains("inactive");
+
+    if(!isMovileMenuClosed){
+        menuMobile.classList.add("inactive")
+    }
+    aside.classList.toggle("inactive");
+    //hacer que el menu desaparesca, aun que no es necesario nos sirveria de practica
 }
