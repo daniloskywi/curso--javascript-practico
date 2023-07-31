4ee1c93d3f57158b4fbed73ec5d02ed9e9ba0394
Danilosky <danilohei2002@gmail.com>
2023-07-31 15:30:01 -0500

    funcion de menu mobile y imagen menu V2

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const manuEmail = document.querySelector(".navbar-email");
 const DesktopMenu = document.querySelector(".desktop-menu");
+const IconButton = document.querySelector(".menu");
+const menuMobile = document.querySelector(".mobile-menu");
 
 manuEmail.addEventListener("click", toggleDesktopMenu);
+IconButton.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu(){
     DesktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu(){
+    menuMobile.classList.toggle("inactive");
 }
