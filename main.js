b421bdd8cb6d1c8c5e1c428e4629e96e6f676b25
Danilosky <danilohei2002@gmail.com>
2023-08-01 17:19:10 -0500

    Fucion de archivos, y maquetatisacion de js V4

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const aside = document.querySelector(".product-detail");
 const IconButton = document.querySelector(".menu");
 const menuMobile = document.querySelector(".mobile-menu");
 
+const carsContainer = document.querySelector(".cards-container")
+
 manuEmail.addEventListener("click", toggleDesktopMenu);
 IconButton.addEventListener("click", toggleMobileMenu);
 menuCarrito.addEventListener("click", toggleCarritoDeCompras)
@@ -42,3 +44,78 @@ function toggleCarritoDeCompras (){
     aside.classList.toggle("inactive");
     //hacer que el menu desaparesca, aun que no es necesario nos sirveria de practica
 }
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    precio: 120,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+productList.push({
+    name: "cafetera",
+    precio: 200,
+    imagen:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+productList.push({
+    name: "Portatil",
+    precio: 600,
+    imagen:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+function renderProducts(Array){
+   
+for (product of Array){
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", product.imagen)
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product-info");
+    const productPrecio = document.createElement("p");
+    productPrecio.innerHTML = "$" + product.precio;
+    const productName = document.createElement("p");
+    productName.innerHTML = "name:" + product.name;
+
+    productDiv.appendChild(productPrecio)
+    productDiv.appendChild(productName)
+
+    const productFigure = document.createElement("figure");
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productFigure.appendChild(productImg)
+
+    productInfo.appendChild(productDiv)
+    productInfo.appendChild(productFigure)
+
+    productCard.appendChild(img)
+    productCard.appendChild(productInfo)
+
+    carsContainer.appendChild(productCard)
+};
+ /*
+<div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>
+
+    </div>
+*/
+}
+renderProducts(productList);
+
